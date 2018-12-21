a61e24eee69dfd204a0ea7353b8359e217e417a2
izatkhamiyev <izat0011@gmail.com>
2018-12-21 17:59:26 +0600

    react redux

--- src/App.js
+++ src/App.js
@@ -2,17 +2,23 @@ import React, { Component } from 'react';
 import Main from './components/MainComponent';
 import './App.css'
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
 
+const state = ConfigureStore();
 class App extends Component {
 
   render() {
 
     return (
-      <BrowserRouter>
-        <div>
-          <Main />
-        </div>
-      </BrowserRouter>
+      <Provider store={state}>
+        <BrowserRouter>
+          <div>
+            <Main />
+          </div>
+        </BrowserRouter>
+      </Provider>
+
     );
 
   }
